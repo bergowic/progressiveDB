761dff729eac97be701ffa73ac8196794595808f
lberg <ljberg@gmx.de>
2019-01-03 16:38:18 +0100

    [Server] Hook Meta

---
+++ progressive-db/src/main/java/de/tuda/progressive/db/ProgressiveMeta.java
@@ -0,0 +1,175 @@
+package de.tuda.progressive.db;
+
+import de.tuda.progressive.db.statement.ProgressiveStatement;
+import de.tuda.progressive.db.statement.ProgressiveStatementFactory;
+import org.apache.calcite.avatica.MissingResultsException;
+import org.apache.calcite.avatica.NoSuchStatementException;
+import org.apache.calcite.avatica.jdbc.JdbcMeta;
+import org.apache.calcite.avatica.jdbc.StatementInfo;
+import org.apache.calcite.avatica.metrics.MetricsSystem;
+import org.apache.calcite.avatica.remote.TypedValue;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class PMeta extends JdbcMeta {
+
+	private final ConcurrentMap<Integer, ProgressiveStatement> statements = new ConcurrentHashMap<>();
+
+	private final ProgressiveStatementFactory statementFactory;
+
+	public PMeta(String url, ProgressiveStatementFactory statementFactory) throws SQLException {
+		super(url);
+
+		this.statementFactory = statementFactory;
+	}
+
+	public PMeta(String url, String user, String password, ProgressiveStatementFactory statementFactory) throws SQLException {
+		super(url, user, password);
+
+		this.statementFactory = statementFactory;
+	}
+
+	public PMeta(String url, Properties info, ProgressiveStatementFactory statementFactory) throws SQLException {
+		super(url, info);
+
+		this.statementFactory = statementFactory;
+	}
+
+	public PMeta(String url, Properties info, MetricsSystem metrics, ProgressiveStatementFactory statementFactory) throws SQLException {
+		super(url, info, metrics);
+
+		this.statementFactory = statementFactory;
+	}
+
+	@Override
+	public StatementHandle prepare(ConnectionHandle ch, String sql, long maxRowCount) {
+		System.out.println(sql);
+
+		if (sql.toUpperCase().startsWith("SELECT STREAM")) {
+			ProgressiveStatement statement = statementFactory.prepare(sql);
+
+			try {
+				StatementHandle handle = new StatementHandle(ch.id, getStatementIdGenerator().getAndIncrement(), signature(statement.getMetaData()));
+				statements.putIfAbsent(handle.id, statement);
+				return handle;
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			return super.prepare(ch, sql, maxRowCount);
+		}
+	}
+
+	@Override
+	public StatementHandle createStatement(ConnectionHandle ch) {
+		return super.createStatement(ch);
+	}
+
+	@Override
+	public ExecuteResult prepareAndExecute(StatementHandle h, String sql, long maxRowCount, int maxRowsInFirstFrame, PrepareCallback callback) throws NoSuchStatementException {
+		ExecuteResult result = hookPrepareAndExecute(h, sql, maxRowCount, maxRowsInFirstFrame);
+		if (result != null) {
+			return result;
+		}
+
+		return super.prepareAndExecute(h, sql, maxRowCount, maxRowsInFirstFrame, callback);
+	}
+
+	@Override
+	public ExecuteResult execute(StatementHandle h, List<TypedValue> parameterValues, long maxRowCount) throws NoSuchStatementException {
+		return super.execute(h, parameterValues, maxRowCount);
+	}
+
+	@Override
+	public ExecuteResult execute(StatementHandle h, List<TypedValue> parameterValues, int maxRowsInFirstFrame) throws NoSuchStatementException {
+		ProgressiveStatement statement = statements.get(h.id);
+		if (statement == null) {
+			return super.execute(h, parameterValues, maxRowsInFirstFrame);
+		}
+
+		return execute(h, statement);
+	}
+
+	@Override
+	public Frame fetch(StatementHandle h, long offset, int fetchMaxRowCount) throws NoSuchStatementException, MissingResultsException {
+		ProgressiveStatement statement = statements.get(h.id);
+		if (statement == null) {
+			return super.fetch(h, offset, fetchMaxRowCount);
+		}
+
+		try {
+			return createFrame(offset, statement.isDone(), statement.getResultSet());
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void closeStatement(StatementHandle h) {
+		ProgressiveStatement statement = statements.get(h.id);
+		if (statement == null) {
+			super.closeStatement(h);
+		} else {
+			statement.close();
+		}
+	}
+
+	private void assertStatementExists(StatementHandle h) throws NoSuchStatementException {
+		final StatementInfo info = getStatementCache().getIfPresent(h.id);
+		if (info == null) {
+			throw new NoSuchStatementException(h);
+		}
+	}
+
+	private ExecuteResult hookPrepareAndExecute(StatementHandle h, String sql, long maxRowCount, int maxRowsInFirstFrame) throws NoSuchStatementException {
+		assertStatementExists(h);
+
+		if (sql.toUpperCase().startsWith("SELECT STREAM")) {
+			ProgressiveStatement statement = statementFactory.prepare(sql);
+			statements.putIfAbsent(h.id, statement);
+
+			return execute(h, statement);
+		}
+
+		return null;
+	}
+
+	private ExecuteResult execute(StatementHandle h, ProgressiveStatement statement) {
+		ResultSet resultSet = statement.getResultSet();
+
+		try {
+			Frame frame = createFrame(0, statement.isDone(), resultSet);
+			MetaResultSet result = MetaResultSet.create(h.connectionId, h.id, false, signature(statement.getMetaData()), frame);
+			return new ExecuteResult(Collections.singletonList(result));
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Frame createFrame(long offset, boolean done, ResultSet resultSet) throws SQLException {
+		if (resultSet == null) {
+			return Frame.create(offset, true, Collections.emptyList());
+		}
+
+		final int columnCount = resultSet.getMetaData().getColumnCount();
+		final List<Object> rows = new ArrayList<>();
+
+		while (resultSet.next()) {
+			Object[] columns = new Object[columnCount];
+			for (int i = 0; i < columnCount; i++) {
+				columns[i] = resultSet.getObject(i + 1);
+			}
+			rows.add(columns);
+			offset++;
+		}
+
+		return Frame.create(offset, done, rows);
+	}
+}
