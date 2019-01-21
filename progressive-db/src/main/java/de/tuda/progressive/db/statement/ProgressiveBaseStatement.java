1d23d8c743f8654b2c3a7034d633e8e054c94e51
lberg <ljberg@gmx.de>
2019-01-21 15:00:18 +0100

    [Server] Add ProgressiveBaseStatement

--- progressive-db/src/main/java/de/tuda/progressive/db/statement/ProgressiveBaseStatement.java
+++ progressive-db/src/main/java/de/tuda/progressive/db/statement/ProgressiveBaseStatement.java
@@ -3,6 +3,9 @@ package de.tuda.progressive.db.statement;
 import de.tuda.progressive.db.driver.DbDriver;
 import de.tuda.progressive.db.model.Partition;
 import de.tuda.progressive.db.util.SqlUtils;
+import org.apache.calcite.sql.SqlSelect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,19 +13,21 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SimpleStatement implements ProgressiveStatement {
+public abstract class ProgressiveBaseStatement implements ProgressiveStatement {
+
+	private static final Logger log = LoggerFactory.getLogger(ProgressiveBaseStatement.class);
 
 	private static final ExecutorService executor = Executors.newCachedThreadPool();
 
-	private final StatementContext context;
+	private final DbDriver driver;
 
 	private final List<Partition> partitions;
+
+	private final Connection tmpConnection;
 
 	private PreparedStatement preparedStatement;
 
@@ -32,19 +37,20 @@ public class SimpleStatement implements ProgressiveStatement {
 
 	private long readPartitions;
 
-	private ResultSetMetaData metaData;
+	protected final ResultSetMetaData metaData;
 
-	private List<Object[]> results = new ArrayList<>();
+	private boolean isClosed;
 
-	public SimpleStatement(
+	public ProgressiveBaseStatement(
 			DbDriver driver,
 			Connection connection,
 			Connection tmpConnection,
 			StatementContext context,
 			List<Partition> partitions
 	) {
-		this.context = context;
+		this.driver = driver;
 		this.partitions = partitions;
+		this.tmpConnection = tmpConnection;
 
 		try {
 			try (Statement statement = tmpConnection.createStatement()) {
@@ -53,7 +59,7 @@ public class SimpleStatement implements ProgressiveStatement {
 
 			preparedStatement = connection.prepareStatement(driver.toSql(context.getSelectSource()));
 			tmpInsertStatement = tmpConnection.prepareStatement(driver.toSql(context.getInsertCache()));
-			tmpSelectStatement = tmpConnection.prepareStatement(driver.toSql(context.getSelectCache()));
+			setTmpSelectStatement(context.getSelectCache());
 
 			metaData = new ResultSetMetaDataWrapper(tmpSelectStatement.getMetaData());
 		} catch (SQLException e) {
@@ -71,6 +77,12 @@ public class SimpleStatement implements ProgressiveStatement {
 		executor.submit(() -> {
 			try {
 				for (Partition partition : partitions) {
+					synchronized (this) {
+						if (isClosed) {
+							break;
+						}
+					}
+
 					query(partition);
 				}
 			} catch (Throwable t) {
@@ -81,72 +93,44 @@ public class SimpleStatement implements ProgressiveStatement {
 	}
 
 	private void query(Partition partition) {
+		log.info("query next partition: {}", partition.getId());
 		try {
 			preparedStatement.setInt(1, partition.getId());
 
 			ResultSet resultSet = preparedStatement.executeQuery();
+
+			log.info("data received");
 			addResult(resultSet, partition);
+			log.info("received data handled");
+
+			synchronized (this) {
+				readPartitions++;
+			}
+
+			queryHandled(partition);
 		} catch (SQLException e) {
 			//TODO
 			throw new RuntimeException(e);
 		}
 	}
 
+	protected abstract void queryHandled(Partition partition);
+
 	private void addResult(ResultSet resultSet, Partition partition) {
 		try {
 			int internalCount = preparedStatement.getMetaData().getColumnCount();
-			while (resultSet.next()) {
+			while (!resultSet.isClosed() && resultSet.next()) {
 				for (int i = 1; i < internalCount; i++) {
 					tmpInsertStatement.setObject(i, resultSet.getObject(i));
 				}
 				tmpInsertStatement.setInt(internalCount, partition.getId());
 				tmpInsertStatement.executeUpdate();
 			}
-
-			synchronized (this) {
-				readPartitions++;
-
-				int pos = 1;
-				for (Aggregation aggregation : context.getAggregations()) {
-					switch (aggregation) {
-						case COUNT:
-						case SUM:
-							tmpSelectStatement.setDouble(pos++, (double) readPartitions / (double) partitions.size());
-							break;
-					}
-				}
-				tmpSelectStatement.setInt(pos, partition.getId());
-
-				ResultSet resultSet1 = tmpSelectStatement.executeQuery();
-				while (resultSet1.next()) {
-					Object[] row = new Object[metaData.getColumnCount()];
-					for (int i = 1; i <= row.length; i++) {
-						row[i - 1] = resultSet1.getObject(i);
-					}
-					results.add(row);
-				}
-				notify();
-			}
-
 		} catch (SQLException e) {
 			//TODO
-			throw new RuntimeException(e);
+			e.printStackTrace();
+//			throw new RuntimeException(e);
 		}
-	}
-
-	@Override
-	public synchronized ResultSet getResultSet() {
-		if (results.isEmpty() && !isDone()) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				// do nothing
-			}
-		}
-
-		ResultSet resultSet = new ProgressiveResultSet(metaData, new LinkedList<>(results));
-		results.clear();
-		return resultSet;
 	}
 
 	@Override
@@ -161,9 +145,23 @@ public class SimpleStatement implements ProgressiveStatement {
 
 	@Override
 	public synchronized void close() {
+		isClosed = true;
+
 		SqlUtils.closeSafe(preparedStatement);
 		SqlUtils.closeSafe(tmpInsertStatement);
 		SqlUtils.closeSafe(tmpSelectStatement);
-		notify();
+	}
+
+	protected PreparedStatement getTmpSelectStatement() {
+		return tmpSelectStatement;
+	}
+
+	protected void setTmpSelectStatement(SqlSelect select) {
+		try {
+			tmpSelectStatement = tmpConnection.prepareStatement(driver.toSql(select));
+		} catch (SQLException e) {
+			// TODO
+			throw new RuntimeException(e);
+		}
 	}
 }
---
+++ progressive-db/src/main/java/de/tuda/progressive/db/statement/ProgressiveSelectStatement.java
@@ -0,0 +1,87 @@
+package de.tuda.progressive.db.statement;
+
+import de.tuda.progressive.db.driver.DbDriver;
+import de.tuda.progressive.db.model.Partition;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ProgressiveSelectStatement extends ProgressiveBaseStatement {
+
+	private final StatementContext context;
+
+	private final List<Partition> partitions;
+
+	private List<Object[]> results = new ArrayList<>();
+
+	public ProgressiveSelectStatement(
+			DbDriver driver,
+			Connection connection,
+			Connection tmpConnection,
+			StatementContext context,
+			List<Partition> partitions
+	) {
+		super(driver, connection, tmpConnection, context, partitions);
+
+		this.context = context;
+		this.partitions = partitions;
+	}
+
+	@Override
+	protected synchronized void queryHandled(Partition partition) {
+		final PreparedStatement tmpSelectStatement = getTmpSelectStatement();
+
+		try {
+			int pos = 1;
+			for (Aggregation aggregation : context.getAggregations()) {
+				switch (aggregation) {
+					case COUNT:
+					case SUM:
+						tmpSelectStatement.setDouble(pos++, (double) getReadPartitions() / (double) partitions.size());
+						break;
+				}
+			}
+			tmpSelectStatement.setInt(pos, partition.getId());
+
+			ResultSet resultSet = tmpSelectStatement.executeQuery();
+			while (resultSet.next()) {
+				Object[] row = new Object[metaData.getColumnCount()];
+				for (int i = 1; i <= row.length; i++) {
+					row[i - 1] = resultSet.getObject(i);
+				}
+				results.add(row);
+			}
+			notify();
+		} catch (SQLException e) {
+			// TODO
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public synchronized ResultSet getResultSet() {
+		if (results.isEmpty() && !isDone()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+		}
+
+		ResultSet resultSet = new ProgressiveResultSet(metaData, new LinkedList<>(results));
+		results.clear();
+		return resultSet;
+	}
+
+	@Override
+	public synchronized void close() {
+		super.close();
+
+		notify();
+	}
+}
