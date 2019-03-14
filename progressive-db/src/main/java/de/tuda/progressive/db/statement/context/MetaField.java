d629227aa9b5e2a3c7c25b579de052965b68caa0
lberg <ljberg@gmx.de>
2019-03-14 01:43:19 +0100

    [Server] Fix MetaField

--- progressive-db/src/main/java/de/tuda/progressive/db/statement/context/MetaField.java
+++ progressive-db/src/main/java/de/tuda/progressive/db/statement/context/MetaField.java
@@ -1,27 +1,28 @@
 package de.tuda.progressive.db.statement.context;
 
 public enum MetaField {
-	NONE(false, false),
-	AVG(false, false),
-	COUNT(false, true),
-	SUM(false, true),
-	PARTITION(true, true),
-	PROGRESS(true, true);
+  NONE(false, false),
+  AVG(false, false),
+  COUNT(false, true),
+  SUM(false, true),
+  FUTURE(false, false),
+  PARTITION(true, true),
+  PROGRESS(true, true);
 
-	private final boolean function;
+  private final boolean function;
 
-	private final boolean substitute;
+  private final boolean substitute;
 
-	MetaField(boolean function, boolean substitute) {
-		this.function = function;
-		this.substitute = substitute;
-	}
+  MetaField(boolean function, boolean substitute) {
+    this.function = function;
+    this.substitute = substitute;
+  }
 
-	public boolean isFunction() {
-		return function;
-	}
+  public boolean isFunction() {
+    return function;
+  }
 
-	public boolean isSubstitute() {
-		return substitute;
-	}
+  public boolean isSubstitute() {
+    return substitute;
+  }
 }
