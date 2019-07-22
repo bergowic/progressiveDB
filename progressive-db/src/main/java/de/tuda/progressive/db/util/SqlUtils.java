12db5b98992fdf2566ef5ada75e7ad16c221c579
lberg <ljberg@gmx.de>
2019-07-22 14:13:31 +0200

    [Server] Support Decimal type for column

--- progressive-db/src/main/java/de/tuda/progressive/db/util/SqlUtils.java
+++ progressive-db/src/main/java/de/tuda/progressive/db/util/SqlUtils.java
@@ -24,6 +24,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -88,6 +89,9 @@ public class SqlUtils {
     SqlTypeName sqlType;
 
     switch (type) {
+      case Types.DECIMAL:
+        sqlType = SqlTypeName.DECIMAL;
+        break;
       case Types.INTEGER:
         sqlType = SqlTypeName.INTEGER;
         break;
